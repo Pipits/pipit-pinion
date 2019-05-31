587b540b92235f3979166565edaf25769387017e
Hussein Al Hammad <husseinalhammad@gmail.com>
2019-05-31 17:11:49 +0400

    Remove str_lreplace()

--- pipit_pinion/lib/PipitPinion_Helper.class.php
+++ pipit_pinion/lib/PipitPinion_Helper.class.php
@@ -265,16 +265,4 @@ class PipitPinion_Helper {
 	
 
 
-	/**
-	 * Not used. Remove?
-	 */
-	function str_lreplace($search, $replace, $subject) {
-		$pos = strrpos($subject, $search);
-	
-		if($pos !== false) {
-			$subject = substr_replace($subject, $replace, $pos, strlen($search));
-		}
-	
-		return $subject;
-	}
 }
